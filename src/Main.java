faf05d6aefccc0f8290711f8e554639f142553d7
mailong05 <vanlong816458@gmail.com>
2024-08-11 13:19:23 +0700

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Van Long!");
+        System.out.println("Hello Mai Van Long!");
         System.out.println("Welcome to the DHKHMT19A!");
     }
 }
