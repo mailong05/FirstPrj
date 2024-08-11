167d515a6e6c67fc336b6187bb0901a75032892c
mailong05 <vanlong816458@gmail.com>
2024-08-11 13:26:33 +0700

    Welcome.java

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,6 +1,6 @@
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Hello Van Long!");
+        System.out.println("Hello Mai Van Long!");
         System.out.println("Welcome to the DHKHMT19A!");
     }
 }
